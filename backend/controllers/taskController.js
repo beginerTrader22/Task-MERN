459e972308ec6fdf098baa22ea272331d331ebe2
Marvi <maravflavia@gmail.com>
2025-05-03 11:54:48 +0200

    back-end controllers and database connection

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,17 +1,27 @@
-const getTasks = (req, res) =>{
-    res.status(200).json({message: 'Get all tasks'})
-}
+const asyncHandler = require('express-async-handler');
+const Task = require('../models/taskmodel')
 
-const setTasks = (req, res) =>{
-    res.status(200).json({message: 'Get all tasks'})
-}
 
-const updateTask = (req, res) =>{
+const getTasks = asyncHandler(async(req, res) =>{
+    const tasks = await Task.find()
+    res.status(200).json(tasks)
+})
+
+const setTasks = asyncHandler(async(req, res) =>{
+    if(!req.body || req.body.text){
+        res.status(400)
+        throw new Error('Please enter a task');
+    }
+
+    res.status(200).json({message: 'Get all tasks'})
+})
+
+const updateTask = asyncHandler(async(req, res) =>{
     res.status(200).json({message: `Task ${req.params.id} updated`})
-}
+})
 
-const deleteTask = (req, res) =>{
+const deleteTask = asyncHandler(async(req, res) =>{
     res.status(200).json({message: `Task ${req.params.id} deleted`})
-}
+})
 
 module.exports = {getTasks , setTasks, updateTask, deleteTask}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-
+const connectDB = require('./connect/database.js')
 const port = process.env.PORT || 5000;
+const {errorHandler} =require('./middleware/errorMidleware.js')
+
 const app = express();
 
 
 //Gets data from front end thorugh the request and sends it back via resposne
 app.use('/api/tasks' , require('./routes/taskRoute.js'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(errorHandler)
+
+connectDB();
 
 app.listen(port, ()=> console.log(`server is listening on ${port}`));
 

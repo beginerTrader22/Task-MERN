35b64cf73bdb688a576b69b77cc50c3fd5e649e2
Marvi <maravflavia@gmail.com>
2025-05-24 11:17:57 +0200

    install react and configure frontend

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  console.log(formData);
+
+  return (
+    <>
+      <section className="heading">
+        <h1> {<FaSignInAlt />} Login</h1>
+        <p>Please Log In</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onsubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter Password"
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Login
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
